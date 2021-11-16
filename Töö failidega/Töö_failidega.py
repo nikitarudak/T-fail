87d7bcfa6ebf00e04421a29cbc4b77d58d98cac4
Nikita Rudak <nikitarudak07@gmail.com>
2021-11-16 09:55:49 +0200

    fail

--- Töö failidega/Töö_failidega.py
+++ Töö failidega/Töö_failidega.py
@@ -1,1 +1,16 @@
-Всем привет!
+file_read=open("TextFile1.txt","r",encoding="utf-8-sig")
+text=file_read.read()
+print(text)
+file_read.close()
+file_write=open("TextFile1.txt","a",encoding="utf-8-sig")
+text=input("Mis tekst on vaja lisada: ")
+file_write.write("\n"+text)
+file_write.close()
+with open("TextFile1.txt","r",encoding="utf-8-sig") as reader:
+    print(reader.read())
+with open("NewFile2.txt","x") as f:
+    for i in range(5):
+        text=input("Mis tekst on vaja lisada: ")
+        f.write("\n"+text)
+with open("NewFile2.txt","r") as f:
+    print(f.read())
